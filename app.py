12e7b55970c4f31d6b4e65637131e4853f599c02
Ganesh <ganeshvaka6@gmail.com>
2026-07-17 21:22:14 +0530

    Wire WhatsApp Content Template

--- app.py
+++ app.py
@@ -394,9 +394,8 @@ def submit():
             "ok": True,
             "message": f"""Dear Participant,
 
-       Thank you for registering for our live Movie Night!
-
-        Your registration has been successfully completed. We’re excited to have you join us for this Movie Night.
+ Thank you for registering for our Free IT Testing Live Demo Session!
+Your registration has been successfully completed. We’re excited to have you join us for this interactive session.
 
         Your seat number: {final}
 
